7e48b895373244aab3b6c1450d6ad4e004da4baf
Maxman <shantanur66@gmail.com>
2021-10-10 14:24:38 +0530

    creating port and listening to it running through cmder : node app

--- server/app.js
+++ server/app.js
@@ -2,4 +2,9 @@
 const express=require('express')
 //instantiating the server using express
 const app=express()
+//creating a port for the server to serve at
+//and listen to it
+app.listen(4000,()=>{
+    console.log('Listening for requests on port 4000');
+})
 
