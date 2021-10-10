6be09d20eae2f05aca2b0a9e5d91b510c3d7a164
Maxman <shantanur66@gmail.com>
2021-10-10 20:08:04 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 //adding express dependency
 const express=require('express')
+//object i can use to interact with nodejs,express,js,etc
+const { graphqlHTTP }=require('express-graphql')
 //instantiating the server using express
 const app=express()
+app.use('/graphQL',graphqlHTTP({
+    graphiql:true
+}))
 //creating a port for the server to serve at
 //and listen to it
 //localhost 4000:(port this is going to be served at)
 //will be giving us error because there is really nothing to be
 //served as we dont have endpoint/routes
-app.listen(4000,()=>{
-    console.log('Listening for requests on port 4000')
+app.listen(5000,()=>{
+    console.log('Listening for requests on port 5000')
 })
 
