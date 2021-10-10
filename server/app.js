8684548ab477b5b945a53a94153e9f17d9f2d37c
Maxman <shantanur66@gmail.com>
2021-10-10 19:05:57 +0530

    installed graphql for js and express

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const express=require('express')
 const app=express()
 //creating a port for the server to serve at
 //and listen to it
+//localhost 4000:(port this is going to be served at)
+//will be giving us error because there is really nothing to be
+//served as we dont have endpoint/routes
 app.listen(4000,()=>{
     console.log('Listening for requests on port 4000')
 })
