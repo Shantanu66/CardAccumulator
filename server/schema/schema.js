3a0af66853a17538df968c7353a00049437587ff
Maxman <shantanur66@gmail.com>
2021-10-10 22:52:08 +0530

    update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql=require('graphql')
-//important to use {} as 
+//important to use {} as require('graphql') return with property/method
 const {GraphQLObjectType}=graphql()
 
 //create types
