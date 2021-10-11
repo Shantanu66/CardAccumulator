1b39370fc0f1a295da3ae450b34c8125f47bf46b
Maxman <shantanur66@gmail.com>
2021-10-11 15:34:08 +0530

    creating data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,5 +3,12 @@ const {GraphQLObjectType}=graphql()
 
 //create types
 const UserType=new GraphQLObjectType({
-
+    name:'USER',
+    description:'Documentation for user...',
+    fields:()=>({
+        id:'1234',
+        name:'Shantanu',
+        age:20
+    })
 })
+
