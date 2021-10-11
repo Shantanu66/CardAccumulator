b9d355d12409cf8511fe53548351fc63269cf90a
Maxman <shantanur66@gmail.com>
2021-10-11 23:50:44 +0530

    update resolving

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,40 +1,45 @@
-const graphql=require('graphql')
+const graphql = require('graphql')
 const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
     GraphQLInt,
     GraphQLSchema
-}=graphql()
+} = graphql
 
 //create types
-const UserType=new GraphQLObjectType({
-    name:'USER',
-    description:'Documentation for user...',
-    fields:()=>({
-        id:{type:GraphQLString},
-        name:{type:GraphQLString},
-        age:{type:GraphQLInt}
+const UserType = new GraphQLObjectType({
+    name: 'USER',
+    description: 'Documentation for user...',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 })
 
 //RootQuery creation
-const RootQuery=new GraphQLObjectType({
-    name:'RootQueryType',
-    description:'Root Query desc',
-    fields:{
-        user:{
-            type:UserType,
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    description: 'Root Query desc',
+    fields: {
+        user: {
+            type: UserType,
             //what argument we need to pass with the main query
-            args:{id:{type:GraphQLString}},
-            resolve(parent,args){
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
                 //where we resolve with data
                 //get and return data from a datasource
-
+                let user = {
+                    id: 'ID66',
+                    name: 'Shantanu Roy',
+                    age: 20
+                }
+                return user
             }
         }
     }
 })
-module.exports=new GraphQLSchema({
-    query:RootQuery
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
