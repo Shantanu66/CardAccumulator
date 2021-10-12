ac15ba0af409dbadf1d4f6109f931d8ba6574407
Maxman <shantanur66@gmail.com>
2021-10-12 12:16:27 +0530

    update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,14 @@
 const graphql = require('graphql')
+
+//dummy data
+var usersData=[
+    {id:'1',name:'Alex',age:32},
+    {id:'1',name:'Bella',age:19},
+    {id:'1',name:'Mona',age:20},
+    {id:'1',name:'Joan',age:39},
+    {id:'1',name:'Triss',age:26}
+]
+
 const {
     GraphQLObjectType,
     GraphQLID,
