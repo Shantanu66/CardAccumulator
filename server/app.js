03b5d0a3d3c6344a199043562086c59c966d3f64
Maxman <shantanur66@gmail.com>
2021-10-17 21:11:01 +0530

    connecting our server with mongoDB

--- server/app.js
+++ server/app.js
@@ -3,18 +3,17 @@ const express=require('express')
 //object i can use to interact with nodejs,express,js,etc
 const { graphqlHTTP }=require('express-graphql')
 const schema=require('./schema/schema')
+const mongoose=require('mongoose')
 //instantiating the server using express
 const app=express()
-
 //mongodb+srv://Shantanu:<password>@cardaccumulator.ctoe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
-
-
 //app.use/the server will use the graph API to communicate
 app.use('/graphQL',graphqlHTTP({
     graphiql:true,
     schema:schema
 }))
+//connecting our server with mongoDB
+mongoose.connect(`mongodb+srv://Shantanu:<password>@cardaccumulator.ctoe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 //creating a port for the server to serve at
 //and listen to it
 //localhost 4000:(port this is going to be served at)
