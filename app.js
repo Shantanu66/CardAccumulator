631eacdfc8c01e619e948f8a4a02953430fc687c
Maxman <shantanur66@gmail.com>
2021-10-17 21:36:34 +0530

    connection done to MongoDB error fixed file loc changed

---
+++ app.js
@@ -0,0 +1,29 @@
+//adding express dependency
+const express=require('express')
+//object i can use to interact with nodejs,express,js,etc
+const { graphqlHTTP }=require('express-graphql')
+const schema=require('./server/schema/schema')
+const mongoose=require('mongoose')
+const port=process.env.PORT || 5000
+//instantiating the server using express
+const app=express()
+//mongodb+srv://Shantanu:<password>@cardaccumulator.ctoe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//app.use/the server will use the graph API to communicate
+app.use('/graphQL',graphqlHTTP({
+    graphiql:true,
+    schema:schema
+}))
+//connecting our server with mongoDB
+mongoose.connect(`mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@cardaccumulator.ctoe1.mongodb.net/${process.env.mongoDB}?retryWrites=true&w=majority`,
+{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    app.listen({port:port},()=>{
+        console.log('Listening for requests on port '+port)
+    })
+}).catch((e)=>console.log("Error:::"+e))
+//creating a port for the server to serve at
+//and listen to it
+//localhost 4000:(port this is going to be served at)
+//will be giving us error because there is really nothing to be
+//served as we dont have endpoint/routes
+
+
