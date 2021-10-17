93908879525d21109c5f48ab371911bc1ed854b6
Maxman <shantanur66@gmail.com>
2021-10-17 22:51:57 +0530

    update

---
+++ server/schema/Database/idcard.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+const mongoose_schema=mongoose.Schema
+
+mongoose.set("useFindAndModify",false)
+
+const idcardSchema=new mongoose_schema({
+    name:String,
+    age:Number,
+    mail:String,
+    profession:String
+})
+module.exports=mongoose.model("Holder",holderSchema)
