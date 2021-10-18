8f5ffc780cfba5c24df2c6352e726b1029f86c5d
Maxman <shantanur66@gmail.com>
2021-10-18 13:55:54 +0530

    created idcard schema

--- server/schema/Database/idcard.js
+++ server/schema/Database/idcard.js
@@ -4,9 +4,8 @@ const mongoose_schema=mongoose.Schema
 mongoose.set("useFindAndModify",false)
 
 const idcardSchema=new mongoose_schema({
-    name:String,
-    age:Number,
-    mail:String,
-    profession:String
+    title:String,
+    description:String,
+    cardnumber:Number
 })
 module.exports=mongoose.model("Idcard",idcardSchema)
