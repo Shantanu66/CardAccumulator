95bd8ab432ef60bf2fa0705300df3d4b5ef0b5bc
Maxman <shantanur66@gmail.com>
2021-10-21 23:59:41 +0530

    save

--- flutter_client/card_accumulator/lib/splashscreen.dart
+++ flutter_client/card_accumulator/lib/splashscreen.dart
@@ -22,32 +22,43 @@ class _MySplashState extends State<MySplash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SplashScreen(
-        seconds: 2,
-        //navigateAfterSeconds: HomeScreenState(),
-        title: Text(
-          'CARD',
-          style: TextStyle(
-            // ignore: prefer_const_literals_to_create_immutables
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1.7,
-            fontSize: 75,
-            color: Colors.white,
+      body: Stack(
+        children: [
+          SplashScreen(
+            seconds: 3,
+            //navigateAfterSeconds: HomeScreenState(),
+            title: Text(
+              'CARD',
+              style: TextStyle(
+                // ignore: prefer_const_literals_to_create_immutables
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.7,
+                fontSize: 75,
+                color: Colors.white,
+              ),
+            ),
+            gradientBackground: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              // ignore: prefer_const_literals_to_create_immutables
+              colors: [
+                Color(0xFF4A148C),
+                Color(0xFF6A1B9A),
+                Color(0xFF7B1FA2),
+                Color(0xFF8E24AA),
+              ],
+            ),
+            useLoader: false,
           ),
-        ),
-        gradientBackground: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          // ignore: prefer_const_literals_to_create_immutables
-          colors: [
-            Color(0xFF4A148C),
-            Color(0xFF6A1B9A),
-            Color(0xFF7B1FA2),
-            Color(0xFF8E24AA),
-          ],
-        ),
-        useLoader: false,
+          Center(
+            child: SpinKitCubeGrid(color: Colors.white,
+            size: 32.0,
+            duration: const Duration(milliseconds: 700),
+            ),
+          )
+        ],
       ),
+      
     );
   }
 }
