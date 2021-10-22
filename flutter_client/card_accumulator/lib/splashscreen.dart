f60c39cff5020333bdb02f5d5684cfba8a21bcc2
Maxman <shantanur66@gmail.com>
2021-10-22 18:11:25 +0530

    final

--- flutter_client/card_accumulator/lib/splashscreen.dart
+++ flutter_client/card_accumulator/lib/splashscreen.dart
@@ -26,7 +26,7 @@ class _MySplashState extends State<MySplash> {
     return Stack(
         children: [
           SplashScreen(
-            seconds: 4,
+            seconds: 3,
             navigateAfterSeconds: HomeScreenState(),
             title: Text(
               'CARDKEEP',
