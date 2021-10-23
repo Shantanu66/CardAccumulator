e9128433a35a15fff975c0106f94a30ff1b30a6a
Maxman <shantanur66@gmail.com>
2021-10-23 21:39:05 +0530

    update showing queried data into our listview

--- flutter_client/card_accumulator/lib/screens/holder_page.dart
+++ flutter_client/card_accumulator/lib/screens/holder_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class HoldersScreen extends StatefulWidget {
   const HoldersScreen({Key? key}) : super(key: key);
@@ -8,60 +9,83 @@ class HoldersScreen extends StatefulWidget {
 }
 
 class _HoldersScreenState extends State<HoldersScreen> {
-  var _controller = ScrollController();
-  ScrollPhysics _physics = ClampingScrollPhysics();
+  final _controller = ScrollController();
+  ScrollPhysics _physics = const ClampingScrollPhysics();
 
   @override
   void initState() {
     super.initState();
     _controller.addListener(() {
       if (_controller.position.pixels <= 56 &&
-      _controller.position.pixels >= 56)
-        setState(() => _physics = ClampingScrollPhysics());
-      else
-        setState(() => _physics = BouncingScrollPhysics());
+          _controller.position.pixels >= 56) {
+        setState(() => _physics = const ClampingScrollPhysics());
+      } else {
+        setState(() => _physics = const BouncingScrollPhysics());
+      }
     });
   }
 
+  String QUERY = """
+      query{
+          holders{
+              name
+              id
+              profession
+              age
+              }
+          }
+  """;
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      controller: _controller,
-      physics: _physics,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Stack(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(
-                  bottom: 23, left: 10, right: 10, top: 20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                /*boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 10),
-                    color: Colors.grey.shade300,
-                    blurRadius: 30,
-                  )
-                ],*/
-              ),
-              padding: const EdgeInsets.all(20),
-              child: InkWell(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Row(
-                        children: const [Text("Holder")],
+    return Query(
+      options: QueryOptions(document: gql(QUERY)),
+      builder: (result, {fetchMore, refetch}) {
+        if(result.isLoading){
+          return const CircularProgressIndicator();
+        }
+        return ListView.builder(
+          controller: _controller,
+          physics: _physics,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                      bottom: 23, left: 19, right: 19, top: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    // ignore: prefer_const_literals_to_create_immutables
+                    boxShadow: [
+                      // ignore: prefer_const_constructors
+                      BoxShadow(
+                        offset: const Offset(0, 8),
+                        color: Colors.black,
+                        blurRadius: 15,
                       )
                     ],
                   ),
-                ),
-              ),
-            )
-          ],
+                  padding: const EdgeInsets.all(20),
+                  child: InkWell(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [Text("Holder")],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            );
+          },
         );
       },
     );
