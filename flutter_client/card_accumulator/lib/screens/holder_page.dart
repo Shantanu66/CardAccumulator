12a002baeabf54651d2b10c5ff6e294858302832
Maxman <shantanur66@gmail.com>
2021-10-23 22:37:27 +0530

    update

--- flutter_client/card_accumulator/lib/screens/holder_page.dart
+++ flutter_client/card_accumulator/lib/screens/holder_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class HoldersScreen extends StatefulWidget {
@@ -10,15 +12,15 @@ class HoldersScreen extends StatefulWidget {
 
 class _HoldersScreenState extends State<HoldersScreen> {
   final _controller = ScrollController();
-  ScrollPhysics _physics = const ClampingScrollPhysics();
+  ScrollPhysics _physics =
+      const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
 
   @override
   void initState() {
     super.initState();
     _controller.addListener(() {
-      if (_controller.position.pixels <= 56 &&
-          _controller.position.pixels >= 56) {
-        setState(() => _physics = const ClampingScrollPhysics());
+      if (_controller.position.pixels <= 56) {
+        setState(() => _physics = const BouncingScrollPhysics());
       } else {
         setState(() => _physics = const BouncingScrollPhysics());
       }
@@ -46,54 +48,83 @@ class _HoldersScreenState extends State<HoldersScreen> {
           return const CircularProgressIndicator();
         }
         HolderData = result.data!["holders"];
-        return ListView.builder(
-          controller: _controller,
-          physics: _physics,
-          itemCount: HolderData.length,
-          itemBuilder: (context, index) {
-            final holder = HolderData[index];
-            return Stack(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                      bottom: 23, left: 19, right: 19, top: 20),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    // ignore: prefer_const_literals_to_create_immutables
-                    boxShadow: [
-                      // ignore: prefer_const_constructors
-                      BoxShadow(
-                        offset: const Offset(0, 8),
-                        color: Colors.black,
-                        blurRadius: 15,
+        return (HolderData.isNotEmpty)
+            ? ListView.builder(
+                controller: _controller,
+                physics: _physics,
+                itemCount: HolderData.length,
+                itemBuilder: (context, index) {
+                  final holder = HolderData[index];
+                  return Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                            bottom: 23, left: 19, right: 19, top: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          // ignore: prefer_const_literals_to_create_immutables
+                          boxShadow: [
+                            // ignore: prefer_const_constructors
+                            BoxShadow(
+                              offset: const Offset(0, 8),
+                              color: Colors.black,
+                              blurRadius: 10,
+                            )
+                          ],
+                        ),
+                        padding: const EdgeInsets.all(20),
+                        child: InkWell(
+                          child: Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "${holder["name"]}",
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 8.0),
+                                  child: Text(
+                                    "Profession:${holder[
+                                      "profession"] ?? 'N/A'}",
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 8.0),
+                                  child: Text(
+                                    "Age:${holder["age"] ?? 'N/A'}",
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
                       )
                     ],
+                  );
+                },
+              )
+            // ignore: avoid_unnecessary_containers
+            : Container(
+                child: Center(
+                  child: Text(
+                    "No holders found",
+                    style: GoogleFonts.openSans(
+                        color: Colors.grey.shade200,
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal),
                   ),
-                  padding: const EdgeInsets.all(20),
-                  child: InkWell(
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "${holder["name"]}",
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            );
-          },
-        );
+                ),
+              );
       },
     );
   }
