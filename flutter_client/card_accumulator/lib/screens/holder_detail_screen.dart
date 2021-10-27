ca77a7aa22cea58c1435e92758a014de65eea304
Maxman <shantanur66@gmail.com>
2021-10-27 20:10:19 +0530

    update

--- flutter_client/card_accumulator/lib/screens/holder_detail_screen.dart
+++ flutter_client/card_accumulator/lib/screens/holder_detail_screen.dart
@@ -27,6 +27,22 @@ class _DetailsPageState extends State<DetailsPage> {
 
   List _idcards = [];
   List _bankcards = [];
+  bool _isidcardvisible = false;
+  bool _isbankcardvisible = false;
+
+  void _visibleidcard() {
+    setState(() {
+      _isidcardvisible = true;
+      _isbankcardvisible = false;
+    });
+  }
+
+  void _visiblebankcard() {
+    setState(() {
+      _isidcardvisible = false;
+      _isbankcardvisible = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -98,6 +114,7 @@ class _DetailsPageState extends State<DetailsPage> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        
                         Text(
                           "${widget.holder["name"]}",
                           style: GoogleFonts.raleway(
@@ -106,7 +123,9 @@ class _DetailsPageState extends State<DetailsPage> {
                               color: Colors.white),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(left: 8.0, top: 15.0),
+                          padding: const EdgeInsets.only(left: 8.0,
+                          bottom: 2.0
+                           ),
                           child: Text(
                             "Age : ${widget.holder["age"] ?? 'N/A'}",
                             style: GoogleFonts.raleway(
@@ -114,12 +133,14 @@ class _DetailsPageState extends State<DetailsPage> {
                                 fontWeight: FontWeight.w600,
                                 color: Colors.white),
                           ),
+                          
                         )
                       ],
                     ),
+                    
                     Padding(
                       padding: const EdgeInsets.only(
-                          left: 8.0, top: 1.0, bottom: 1.0),
+                          left: 8.0, top: 2.0, bottom: 1.0),
                       child: Text(
                         "Profession : ${widget.holder["profession"] ?? 'N/A'}",
                         style: GoogleFonts.raleway(
@@ -130,7 +151,7 @@ class _DetailsPageState extends State<DetailsPage> {
                     ),
                     Padding(
                       padding: const EdgeInsets.only(
-                          left: 8.0, top: 0.2, bottom: 12.0),
+                          left: 8.0, top: 1.0),
                       child: Text(
                         "Email : ${widget.holder["mail"] ?? 'N/A'}",
                         style: GoogleFonts.raleway(
@@ -139,8 +160,25 @@ class _DetailsPageState extends State<DetailsPage> {
                             color: Colors.white),
                       ),
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 280.0,
+                          top:0.0),
+                          child: InkWell(
+                            child: Icon(Icons.add_box
+                            ),
+                            onTap: (){},
+                          ),
+                        )
+                        
+                      ],
+                    )
                   ],
+                  
                 ),
+                
                 // ignore: prefer_const_constructors
                 decoration: BoxDecoration(
                   // ignore: prefer_const_constructors
@@ -168,8 +206,9 @@ class _DetailsPageState extends State<DetailsPage> {
                 padding: const EdgeInsets.only(right: 30.0),
                 child: TextButton(
                   onPressed: () {
+                    _visibleidcard();
                     setState(() {
-                      _idcards=widget.holder["idcards"];
+                      _idcards = widget.holder["idcards"];
                     });
                   },
                   child: Text(
@@ -188,7 +227,12 @@ class _DetailsPageState extends State<DetailsPage> {
               Padding(
                 padding: const EdgeInsets.only(left: 20.0),
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _visiblebankcard();
+                    setState(() {
+                      _bankcards = widget.holder["bankcards"];
+                    });
+                  },
                   child: Text(
                     "My Bank cards",
                     style: GoogleFonts.roboto(
@@ -205,7 +249,7 @@ class _DetailsPageState extends State<DetailsPage> {
             ],
           ),
           Visibility(
-            visible: true,
+            visible: _isidcardvisible,
             child: Container(
               height: MediaQuery.of(context).size.height * 0.45,
               child: ListView.builder(
@@ -228,8 +272,8 @@ class _DetailsPageState extends State<DetailsPage> {
                             end: Alignment.bottomCenter,
                             colors: [
                               //Colors.purple.shade300,
-                              Colors.deepPurple,
-                              Colors.purple,
+                              Colors.indigo,
+                              Colors.blue,
 
                               //Colors.deepPurple.shade700
                             ],
@@ -239,7 +283,7 @@ class _DetailsPageState extends State<DetailsPage> {
                             // ignore: prefer_const_constructors
                             BoxShadow(
                               offset: const Offset(2, 6),
-                              color: Colors.purple,
+                              color: Colors.blue,
                               blurRadius: 19,
                               spreadRadius: -2.5,
                             )
@@ -252,11 +296,141 @@ class _DetailsPageState extends State<DetailsPage> {
                             mainAxisSize: MainAxisSize.min,
                             children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text("${data["title"]}"),
+                                  Text(
+                                    "${data["title"]}",
+                                    style: GoogleFonts.raleway(
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.white),
+                                  ),
                                 ],
-                              )
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 8.0, top: 15.0),
+                                child: Text(
+                                  "Description : ${data["description"]}",
+                                  style: GoogleFonts.raleway(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 8.0, top: 15.0),
+                                child: Text(
+                                  "Card Number : ${data["cardnumber"]}",
+                                  style: GoogleFonts.raleway(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 8.0, top: 15.0),
+                                child: Text(
+                                  "DOB : ${data["DOB"]}",
+                                  style: GoogleFonts.raleway(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+          Visibility(
+            visible: _isbankcardvisible,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.45,
+              child: ListView.builder(
+                controller: _controller,
+                physics: _physics,
+                itemCount: _bankcards.length,
+                itemBuilder: (context, index) {
+                  var data = _bankcards[index];
+                  return Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                            bottom: 23, left: 19, right: 19, top: 20),
+                        // ignore: duplicate_ignore
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          // ignore: prefer_const_constructors
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              //Colors.purple.shade300,
+                              Colors.red,
+                              Colors.yellow.shade600,
+
+                              //Colors.deepPurple.shade700
+                            ],
+                          ),
+                          // ignore: prefer_const_literals_to_create_immutables
+                          boxShadow: [
+                            // ignore: prefer_const_constructors
+                            BoxShadow(
+                              offset: const Offset(2, 6),
+                              color: Colors.yellow.shade600,
+                              blurRadius: 19,
+                              spreadRadius: -2.5,
+                            )
+                          ],
+                        ),
+                        padding: const EdgeInsets.all(20),
+                        child: Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("${data["bank"]}",
+                                  style: GoogleFonts.raleway(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white),),
+                                ],
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 8.0, top: 15.0),
+                                child: Text(
+                                  "Validity : ${data["validity"]}",
+                                  style: GoogleFonts.raleway(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 8.0, top: 15.0),
+                                child: Text(
+                                  "Card Number : ${data["number"]}",
+                                  style: GoogleFonts.raleway(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white),
+                                ),
+                              ),
                             ],
                           ),
                         ),
