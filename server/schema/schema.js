99ef38896fc0df34f22b403f815bb24d425cb41d
Maxman <shantanur66@gmail.com>
2022-08-22 23:55:33 +0530

    update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -134,7 +134,7 @@ const RootQuery = new GraphQLObjectType({
             type:new GraphQLList(holderType),
             
             resolve(parent,args){
-                return holder.find({name:"Shantanu"}).id
+                return holder.find({name:"Shantanu"})
                 
             }
         },
