141a1357cb576395d2b6f2e3979f03c838269d10
Maxman <shantanur66@gmail.com>
2022-08-23 01:50:34 +0530

    update

--- app.js
+++ app.js
@@ -3,30 +3,35 @@ const express=require('express')
 //object i can use to interact with nodejs,express,js,etc
 const { graphqlHTTP }=require('express-graphql')
 const schema=require('./server/schema/schema')
-const mongoose=require('mongoose')
+const { startDatabase } = require("./database");
 const cors=require('cors')
 const port=process.env.PORT || 5000
 //instantiating the server using express
 const app=express()
+const context = async () => {
+    const db = await startDatabase();
+  
+    return { db };
+  };
 //mongodb+srv://Shantanu:<password>@cardaccumulator.ctoe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 //app.use/the server will use the graph API to communicate
 //using cors(cross origin resource origin)
 app.use(cors())
 app.use("/graphql",graphqlHTTP({
     graphiql:true,
-    schema:schema
+    schema:schema,
+    context:context,
 }))
-//connecting our server with mongoDB
-mongoose.connect(`mongodb+srv://${ process.env.mongoUserName}:${process.env.mongoUserPassword}@cardaccumulator.ctoe1.mongodb.net/${process.env.mongoDB}?retryWrites=true&w=majority`,
-{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
-    app.listen({port:port},()=>{
-        console.log('Listening for requests on port '+port)
-    })
+app.listen({port:port},()=>{
+    console.log(`Server ready at http://localhost:${port}/graphql`);
 }).catch((e)=>console.log("Error:::"+e))
+//connecting our server with mongoDB
+
+module.exports=app
 //creating a port for the server to serve at
 //and listen to it
 //localhost 4000:(port this is going to be served at)
 //will be giving us error because there is really nothing to be
 //served as we dont have endpoint/routes
 
-module.exports=app
+
--- database.js
+++ database.js
@@ -5,11 +5,7 @@ const app=require('./app.js')
 
 async function startDatabase() {
     mongoose.connect(`mongodb+srv://${ process.env.mongoUserName}:${process.env.mongoUserPassword}@cardaccumulator.ctoe1.mongodb.net/${process.env.mongoDB}?retryWrites=true&w=majority`,
-{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
-    app.listen({port:port},()=>{
-        console.log('Listening for requests on port '+port)
-    })
-}).catch((e)=>console.log("Error:::"+e))
+{useNewUrlParser:true,useUnifiedTopology:true})
 }
 async function stopDatabase(){
     mongoose.connection.close()
