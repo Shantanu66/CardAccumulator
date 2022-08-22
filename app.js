915c001d8e3bc6b6dc1f53f1abfb0f9c23e096ba
Maxman <shantanur66@gmail.com>
2022-08-23 02:40:12 +0530

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app=express()
 //mongodb+srv://Shantanu:<password>@cardaccumulator.ctoe1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 //app.use/the server will use the graph API to communicate
 //using cors(cross origin resource origin)
-const Context = async () => {
+const context = async () => {
     const db = await startDatabase();
   
     return { db };
@@ -19,11 +19,16 @@ const Context = async () => {
 app.use(cors())
 app.use("/graphql",graphqlHTTP({
     graphiql:true,
-    schema:schema,
-    context:Context
+    schema:schema
+    
 }))
 //connecting our server with mongoDB
-
+mongoose.connect(`mongodb+srv://${ process.env.mongoUserName}:${process.env.mongoUserPassword}@cardaccumulator.ctoe1.mongodb.net/${process.env.mongoDB}?retryWrites=true&w=majority`,
+{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    app.listen({port:port},()=>{
+        console.log('Listening for requests on port '+port)
+    })
+}).catch((e)=>console.log("Error:::"+e))
 //creating a port for the server to serve at
 //and listen to it
 //localhost 4000:(port this is going to be served at)
